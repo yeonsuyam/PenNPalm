86dd0f526b345b243794b1c52dcb299bbea39175
Yeonsu Kim <yeonsuyam@gmail.com>
2021-02-23 15:40:01 +0900

    Refine code of drawChar.py

--- sensorTest/drawChar.py
+++ sensorTest/drawChar.py
@@ -19,20 +19,22 @@ port = "/dev/cu.usbserial-AK08KN48"
 baudrate = 115200
 PRINT_SPEED = 0.8
 
-NEXTCHAR = 1
-NEXTWORD = 2
-POINTING = 3
+NEXTSTROKE = 1
+NEXTCHAR = 2
+NEXTWORD = 3
+POINTING = 4
 
-width, height = 50, 128
+widthOfChar, heightOfChar = 100, 256
 
 def visualize(x, y):
-	x, y = 50, 128
+	cnt = 0
+	x, y = widthOfChar//2, heightOfChar//2
 	
 	xList = []
 	yList = []
 
-	img = 255 * np.ones(shape=[height * 2, width*10, 3], dtype=np.uint8)
-	# cv2.imshow("White Blank", img)
+	img = 255 * np.ones(shape=[heightOfChar, widthOfChar*10, 3], dtype=np.uint8)
+	cv2.imshow("White Blank", img)
 
 	numChar = 0
 	flag = 0
@@ -45,35 +47,68 @@ def visualize(x, y):
 			pass
 		
 		else:
+			if numChar >= 16:
+				print("reset canvas")
+				img = 255 * np.ones(shape=[heightOfChar, widthOfChar*10, 3], dtype=np.uint8)
+				cv2.imshow("White Blank", img)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+
+				numChar = 0
+
 			if dx >= 999:
-				numChar += 1
 				flag += 1
+				
+				if flag == NEXTCHAR:
+					numChar += 1
+					print("next char")
+
+				if flag == NEXTWORD:
+					print("reset canvas")
+					img = 255 * np.ones(shape=[heightOfChar, widthOfChar*10, 3], dtype=np.uint8)
+					cv2.imshow("White Blank", img)
+					if cv2.waitKey(1) & 0xFF == ord('q'):
+						break
+
 				continue
 
 			else:
-				if flag == NEXTCHAR:
-					x += numChar * 70
-					y = 128
-					numChar = 0
+				if flag == NEXTSTROKE:
+					x += dx
+					y += dy
+				elif flag == NEXTCHAR:
+					x = (numChar+1) * widthOfChar // 2
+					y = heightOfChar//2
+
+					# x += dx
+					# y += dy
 
 				elif flag == NEXTWORD:
 					# reset canvas
-					img = 255 * np.ones(shape=[height * 2, width*10, 3], dtype=np.uint8)
-					x = width
-					y = height
+					# img = 255 * np.ones(shape=[heightOfChar, widthOfChar*10, 3], dtype=np.uint8)
+					# cv2.imshow("White Blank", img)
+					x, y = widthOfChar//2, heightOfChar//2
+
+
+					numChar = 0
 
 				elif flag == POINTING:
 					# reset canvas
-					img = 255 * np.ones(shape=[height * 2, width*10, 3], dtype=np.uint8)
-					x = width
-					y = height
+					# img = 255 * np.ones(shape=[heightOfChar * 2, widthOfChar*10, 3], dtype=np.uint8)
+					# x = widthOfChar
+					# y = heightOfChar
 					print("TODO: Start Pointing")
 
-				img = cv2.line(img, (x, y), (x+dx, y - dy), (0, 0, 0), 1)
-				x += dx
-				y -= dy
-				cv2.imshow("White Blank", img)
-				
+				else:
+					img = cv2.line(img, (x, y), (x+dx, y - dy), (0, 0, 0), 1)
+					cnt += 1
+					x += dx
+					y -= dy
+
+					if cnt == 10:
+						cv2.imshow("White Blank", img)
+						cnt = 0
+					
 				if cv2.waitKey(1) & 0xFF == ord('q'):
 					break
 
@@ -106,7 +141,7 @@ def main(xData, yData):
 			
 			dx, dy = tuple(int(num) for num in line.split(', '))
 
-			print("dx: ", dx, "dy: ", dy)
+			# print("dx: ", dx, "dy: ", dy)
 
 			xData.put(dx)
 			yData.put(dy)
