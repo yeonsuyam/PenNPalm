73c0ac3a299494f793535a0c9e5203927d9dbe17
Yeonsu Kim <yeonsuyam@gmail.com>
2021-02-24 16:16:24 +0900

    Update drawOnDesktop.py

--- sensorTest/drawOnDesktop.py
+++ sensorTest/drawOnDesktop.py
@@ -1,4 +1,7 @@
 import serial
+from pynput.mouse import Button, Controller
+from multiprocessing import Process, Queue
+import queue
 
 port = "/dev/cu.usbserial-AK08KN48"
 baudrate = 115200
@@ -22,16 +25,59 @@ def init():
     return ser
 
 
-def main():
-	arduino = init()
+def main(xData, yData):
+    arduino = init()
 
-	while True:
-		try:
-			line = arduino.readline().decode('ascii')
-			dx, dy = tuple(int(num) for num in line.split(', '))
+    while True:
+        try:
+            line = arduino.readline().decode('ascii')
+            
+            dx, dy = tuple(int(num) for num in line.split(', '))
 
-			print("dx: ", dx, "dy: ", dy)
+            # print("dx: ", dx, "dy: ", dy)
+
+            xData.put(dx)
+            yData.put(dy)
+
+        except:
+            pass
+
+
+def isTouch(data):
+    if data >= 999:
+        return False
+    else:
+        return True
+
+
+def mouseDraw(xData, yData):
+    isDrawFlag = 2
+
+    while True:
+        try:
+            dx, dy = xData.get(False), yData.get(False)
+            
+        except queue.Empty:
+            pass
+        
+        else:
+            if not isTouch(dx):
+                mouse.release(Button.left)
+                mouse.move(50, 0)
+                isDrawFlag = 2
+
+            else:
+                if isDrawFlag > 0:
+                    isDrawFlag -= 1
+                    if isDrawFlag == 0:
+                        mouse.press(Button.left)
+
+                mouse.move(dx, -dy)
 
 
 if __name__ == "__main__":
-	main()
+    xData, yData = Queue(), Queue()
+    mainProcess = Process(target=main, args=(xData, yData,), daemon=True)
+    mainProcess.start()
+
+    mouseDraw(xData, yData)
